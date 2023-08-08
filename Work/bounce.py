2234d64dc243704d09926ac5a6d5e9606c492b62
Saransh Sood <saranshsood24@gmail.com>
2023-08-08 12:40:15 +0530

    Section 1.2 completed

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+drop_height = 100
+loss_fraction = 3 / 5
+
+hit_num = 1
+
+while hit_num <= 10:  
+    drop_height *= loss_fraction # updates height every bounce
+    print(hit_num, round(drop_height, 4))
+    hit_num += 1 # increases hits
