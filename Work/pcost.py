65f5cc3de369c6c7317dfd914c8fb87e9c90afbb
Saransh Sood <saranshsood24@gmail.com>
2023-08-11 00:33:27 +0530

    Section 1.0 complete

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
-cost  = 0
+import csv 
+import sys
 
-with open('Data/portfolio.csv', 'rt') as file:
-    headers = next(file)
-    for line in file:
-        stock = line.split(',')
-        cost += int(stock[1]) * float(stock[2])
+def portfolio_cost(filename):
+    cost = 0
+    with open(filename, 'rt') as file:
+        reader = csv.reader(file)
+        next(reader) # Filtering headers
+        for row in reader:
+            try:
+                num = int(row[1])
+                value = float(row[2])
+                cost += num * value
+            except ValueError:
+                print('WARNING: Data missing...', row)
 
-print('Total cost', cost)
+    return cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
