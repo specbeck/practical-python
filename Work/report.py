3fd6b4f0d7f4b7e025fecf84b9cf11cfdb35fda4
Saransh Sood <saranshsood24@gmail.com>
2023-08-25 18:01:39 +0530

    Refactor using dictionaries

--- Work/report.py
+++ Work/report.py
@@ -2,16 +2,21 @@
 #
 # Exercise 2.4
 import csv
+from pprint import pprint
 
 def read_portfolio(filename):
     portfolio = []
 
     with open(filename, 'rt') as f:
-        rows = csv.reader(f);
+        rows = csv.reader(f)
         headers = next(rows)
 
-        for row in rows:
-            holding = (row[0], int(row[1]) , float(row[2]))
+        for row in rows:   
+            holding = {headers[0] : row[0], headers[1] : int(row[1]) , headers[2] : float(row[2])}
             portfolio.append(holding)
 
     return portfolio
+\
+
+portfolio = read_portfolio("Data/portfolio.csv")
+pprint(portfolio)
