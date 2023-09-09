f481bc7f1715b8c8633061d54c3e7f7a5d27dc30
Saransh Sood <saranshsood24@gmail.com>
2023-09-09 16:46:42 +0530

    Section 3.3 done

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,15 +4,27 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=","):
+def parse_csv(
+    filename,
+    select=None,
+    types=None,
+    has_headers=True,
+    delimiter=",",
+    silence_errors=True,
+):
     """
     Parse a CSV file into a list of records
     """
+
+    if not has_headers and select:
+        raise RuntimeError("Select argument also requires column headers.")
+
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
 
         # Read the file headers if they exist
-        if has_headers: headers = next(rows)
+        if has_headers:
+            headers = next(rows)
 
         # If a column selector was given, find indices of the specified columns.
         # Also narrow the set of headers used for resulting dictionaries
@@ -21,7 +33,7 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=","
             headers = select
 
         records = []
-        for row in rows:
+        for rowno, row in enumerate(rows, start=1):
             if not row:  # Skip rows with no data
                 continue
 
@@ -31,7 +43,12 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=","
 
             # Perform type conversions if supplied
             if types:
-                row = [func(val) for func, val in zip(types, row)]
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except Exception as e:
+                    if not silence_errors:
+                        print(f"Row {rowno}: Couldn't convert {row}")
+                        print(f"Row {rowno}: {e}")
 
             # If no headers append the tuple
             if has_headers:
