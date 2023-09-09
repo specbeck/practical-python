d1157ec5ac5b842c781f1d007b9fc2a80f3761b7
Saransh Sood <saranshsood24@gmail.com>
2023-09-09 13:44:48 +0530

    Added comments for 3.1

--- Work/report.py
+++ Work/report.py
@@ -28,6 +28,9 @@ def read_portfolio(filename):
 
 
 def read_prices(filename):
+    """
+    Read a CSV file of price data into a dict mapping names to prices.
+    """
     stocks = {}
 
     with open(filename, "rt") as f:
@@ -41,6 +44,7 @@ def read_prices(filename):
 
 
 def get_cost(portfolio):
+    """ Get the total cost of a portfolio."""
     total_cost = 0.0
     for s in portfolio:
         total_cost += s["shares"] * s["price"]
@@ -48,6 +52,7 @@ def get_cost(portfolio):
 
 
 def get_current_value(portfolio, prices):
+    """ Get the current value of stocks in a portfolio based on prices."""
     current_value = 0.0
     for s in portfolio:
         current_value += s["shares"] * prices[s["name"]]
@@ -55,6 +60,10 @@ def get_current_value(portfolio, prices):
 
 
 def make_report(stocks, prices):
+    """
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    """
     report = []
     for stock in stocks:
         stock_name = stock["name"]
@@ -66,6 +75,9 @@ def make_report(stocks, prices):
 
 
 def print_report(report):
+    """
+    Print a nicely formated table from a list of (name, shares, price, change) tuples.
+    """
     headers = ("Name", "Shares", "Price", "Change")
     print("%10s %10s %10s %10s" % headers)
     print(("-" * 10 + " ") * len(headers))
@@ -73,7 +85,11 @@ def print_report(report):
         price = f"${price:.2f}"
         print(f"{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}")
 
+
 def portfolio_report(portfolio_filename, prices_filename):
+    """
+    Make a stock report given portfolio and price data files.
+    """
     portfolio = read_portfolio(portfolio_filename)
     prices = read_prices(prices_filename)
     report = make_report(portfolio, prices)
