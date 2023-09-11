8299952214cb5cc7ae0dbd4a6ad903ab638756ee
Saransh Sood <saranshsood24@gmail.com>
2023-09-12 00:05:30 +0530

    Section 5 complete

--- Work/stock.py
+++ Work/stock.py
@@ -1,6 +1,7 @@
 class Stock:
     """A Stock holding instance having attributes name, shares and price."""
 
+    __slots__ = ('name', '_shares', 'price')
     def __init__(self, name, shares, price) -> None:
         self.name = name
         self.shares = shares
@@ -9,6 +10,17 @@ class Stock:
     def __repr__(self):
         return f"Stock({self.name!r}, {self.shares!r}, {self.price!r})"
 
+    @property 
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Expected an integer")
+        self._shares = value
+
+    @property
     def cost(self):
         """Cost of a stock as shares * price"""
         return self.shares * self.price
